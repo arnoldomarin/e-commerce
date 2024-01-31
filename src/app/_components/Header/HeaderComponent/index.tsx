d6e14774ed3d3c2d78294859a57dc84d27eb98e5
Arnoldo Marin Ahijado <37191387+arnoldomarin@users.noreply.github.com>
2024-01-31 12:02:50 +0100

    Finished navbar setup and create new mobile navbar that needs more work

--- src/app/_components/Header/HeaderComponent/index.tsx
+++ src/app/_components/Header/HeaderComponent/index.tsx
@@ -1,15 +1,28 @@
+'use client';
+
 import { Header } from 'next/dist/lib/load-custom-routes'
 import React from 'react'
 import { Gutter } from '../../Gutter'
 import Link from 'next/link'
+import Image from 'next/image'
+
+import classes from './index.module.scss'
+import { HeaderNav } from '../Nav'
+import MobileNav from '../MobileNav'
+import { noHeaderFooterUrls } from '../../../constants/indext'
+import { usePathname } from 'next/navigation'
 
 const HeaderComponent = ({ header }: { header: Header}) => {
+    const pathname = usePathname();
   return (
-    <nav>
-        <Gutter>
+    <nav className={[classes.header, noHeaderFooterUrls.includes(pathname) && classes.hide].filter(Boolean).join(' ')}>
+        <Gutter className={classes.wrap}>
             <Link href='/'>
-                <img src='../../logo-black.svg' alt='logo'/>
+                <Image src='../../logo-black.svg' alt='logo' width={170} height={50}/>
             </Link>
+
+            <HeaderNav header={header}/>
+            <MobileNav header={header} />
         </Gutter>
     </nav>
   )
